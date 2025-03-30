162f5e3cfbe563b7c27320b8686486f17ca4da97
Andrea Bizzotto <bizz8.99@gmail.com>
2025-03-30 18:17:13 +0200

    feat: add license to create_platform.py script

--- create_platform.py
+++ create_platform.py
@@ -1,4 +1,21 @@
-# Andrea Bizzotto
+# Create a platform in Vitis using the .spfm and .xsa files
+
+"""
+# Copyright (C) 2025  Andrea Bizotto
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 
 # TODO enable the library in the platform
 # TODO create the system project
